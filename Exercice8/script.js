50061ff7c9b12c82ee666b5cb99cdd671c0d8cfb
Melba17 <evabancerowski77@gmail.com>
2024-06-24 09:50:42 +0200

    exo 8

--- Exercice8/script.js
+++ Exercice8/script.js
@@ -1,12 +1,10 @@
 function add(num1, num2) {
-    if (num1 === undefined && num2 === undefined) {
-        return 0;  // Retourne 0 si 'num1' et 'num2' sont indéfinis. 
-        // Selon les besoins, ils peuvent être définis au moment de l'appel/activation de la fonction.
-    } else if (num2 === undefined) {
-        return Number(num1);  // Retourne la valeur numérique de 'num1' si c'est une chaîne de caractère entre '' et si 'num2' est indéfini.
-    } else {
-        return Number(num1) + Number(num2);  // Convertit 'num1' et 'num2' en nombres si ce sont des chaînes caractères et retourne leur somme.
-    }
+    // Conversion des chaînes de caractères en nombres en utilisant Number() si besoin.
+    num1 = Number(num1);
+    num2 = Number(num2);
+
+    // Retourner la somme des deux nombres
+    return num1 + num2;
 }
 
  export default add;
